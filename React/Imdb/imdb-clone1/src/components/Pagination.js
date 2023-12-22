c11038c8ddaaaa400eb2206e8f7c5735bcc3be31
siva <nssp.siva@gmail.com>
2023-12-22 23:39:36 +0530

    updates has done

--- React/Imdb/imdb-clone1/src/components/Pagination.js
+++ React/Imdb/imdb-clone1/src/components/Pagination.js
@@ -4,9 +4,9 @@ const Pagination = (Props) => {
     const {pageNumberProp,next,previous}=Props
   return (
     <div className='flex flex-wrap justify-center'>
-        <div onClick={previous} className='p-4 m-2 rounded-sm hover:scale-110 duration-300 bg-gray-850'>Previous</div>
+        <div onClick={previous} className='p-4 m-2  rounded-sm hover:scale-110 duration-300 bg-gray-850 hover:bg-sky-700 cursor-pointer'>Previous</div>
         <div className='p-4 m-2 rounded-sm '>{pageNumberProp}</div>
-        <div onClick={next} className='p-4 m-2 rounded-sm hover:scale-110 duration-300'>Next</div>
+        <div onClick={next} className='p-4 m-2  rounded-sm hover:scale-110 duration-300 hover:bg-sky-700 cursor-pointer'>Next</div>
     </div>
   )
 }
