f40e56cf09253128b804b4c3c764554bd9e0bb70
siva <nssp.siva@gmail.com>
2024-03-04 23:54:50 +0530

    movies model and movies api is created

--- BMS_clone/server/server.js
+++ BMS_clone/server/server.js
@@ -4,8 +4,10 @@ require('dotenv').config()
 const app=express()
 const dbConfig=require('./cofig/dbConfig')
 const userRouter=require('./routes/userRouter')
+const movieRouter=require('./routes/moviesRouter')
 app.use(express.json())
 app.use('/api/users',userRouter.router)
+app.use('/api/movies',movieRouter.router)
 
 
 app.listen(8080,()=>{
